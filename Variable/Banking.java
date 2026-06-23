eba21a5ee28b4c0fccd97a1ed5b0135c7716c8b4
Pm Pranta <heyiampm@gmail.com>
2026-06-23 20:46:29 +0600

    Code add java

---
+++ Variable/Banking.java
@@ -0,0 +1,10 @@
+public class Banking {
+
+          public static void main(String[] args) {
+                    System.out.println("|--------------------|");
+                    System.out.println(" COMMONWEL TATA BANK ");
+                    System.out.println("|--------------------|");
+                    System.out.println("Date: july 07 , 2026.");
+                    System.out.println("Welcome Back , Super Mario.");
+          }
+}
---
+++ Variable/Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name = "Pranta Mohajan";
+        int roll = 1025;
+        String address = "Chittagong, Bangladesh";
+        boolean isStudent = true;
+        System.out.println("Name: " + name);
+        System.out.println("Roll: " + roll);
+        System.out.println("Address: " + address);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("It's " + isStudent + ", " + name + " is a student at Premier University.");
+    }
+}
